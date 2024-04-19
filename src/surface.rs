bbfafc3303fab886c5ba20c1b9927dbd04de6a9c
veritius <veritiusgaming@gmail.com>
2024-04-19 13:39:05 +1000

    tweak surface shader

--- src/surface.rs
+++ src/surface.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy::render::render_resource::{AsBindGroup, Extent3d, ShaderRef, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages};
+use bevy::render::render_resource::{AsBindGroup, ShaderRef, TextureUsages};
 
 /// [`TextureUsages`] needed for a functional [`Image`] used in drawing wormholes.
 // Using an integer is a hack to get around bitwise OR being unusable in a const context.
@@ -12,87 +12,30 @@ fn correct_usages_test() {
     assert_eq!(WORMHOLE_TEXTURE_USAGES, k);
 }
 
-/// A wormhole.
+/// A wormhole entity. It's recommended that you use [`WormholeBuilder`](crate::WormholeBuilder) to create this component.
 #[derive(Debug, Component, Reflect)]
 #[reflect(Debug, Component)]
 pub struct Wormhole {
     /// The other side of the wormhole.
     pub counterpart: Entity,
-    /// A stencil mask for the surface's opacity.
-    pub mask: Option<Handle<Image>>,
 }
 
-/// A bundle for creating a [`Wormhole`].
-#[derive(Bundle)]
-#[allow(missing_docs)]
-pub struct WormholeBundle {
-    pub transform: TransformBundle,
-    pub visibility: VisibilityBundle,
-    pub wormhole: Wormhole,
-    pub mesh: Handle<Mesh>,
-    pub material: Handle<WormholeMaterial>,
-}
-
-impl WormholeBundle {
-    /// Creates a new [`Wormhole`] with an appropriate [`Mesh`] and [`Image`].
-    /// 
-    /// `dimensions` is in world units. `resolution` is in pixels.
-    /// 
-    /// [`counterpart`](Wormhole::counterpart) will be set to a placeholder value.
-    /// It must be replaced with the entity ID of the other side of the wormhole.
-    pub fn new(
-        dimensions: Vec2,
-        resolution: UVec2,
-        meshes: &mut Assets<Mesh>,
-        images: &mut Assets<Image>,
-        materials: &mut Assets<WormholeMaterial>,
-    ) -> Self {
-        let image = images.add(Image {
-            texture_descriptor: TextureDescriptor {
-                label: None,
-                size: Extent3d {
-                    width: resolution.x,
-                    height: resolution.y,
-                    ..default()
-                },
-                mip_level_count: 1,
-                sample_count: 1,
-                dimension: TextureDimension::D2,
-                format: TextureFormat::Bgra8Unorm,
-                usage: WORMHOLE_TEXTURE_USAGES,
-                view_formats: &[],
-            },
-            ..default()
-        });
-
-        let material = materials.add(WormholeMaterial {
-            texture: image,
-        });
-
-        Self {
-            transform: TransformBundle::default(),
-            visibility: VisibilityBundle::default(),
-            wormhole: Wormhole {
-                counterpart: Entity::PLACEHOLDER,
-                mask: None,
-            },
-            mesh: meshes.add(Plane3d::new(Vec3::Y).mesh().size(dimensions.x, dimensions.y)),
-            material,
-        }
-    }
-}
-
-/// A simple shader for rendering wormhole surfaces.
-/// You can also use [`StandardMaterial`] with the same `Handle<Image>` if you want.
+/// A shader for rendering wormhole surfaces.
+/// 
+/// To work correctly, the mesh's UVs must be in a linear space ranging from `[0.0, 0.0]` to `[1.0, 1.0]`.
 #[derive(Debug, Clone, TypePath, Asset, AsBindGroup)]
 #[allow(missing_docs)]
-pub struct WormholeMaterial {
+pub struct WormholeSurface {
     #[texture(0)]
     #[sampler(1)]
     pub texture: Handle<Image>,
+
+    #[texture(2)]
+    #[sampler(3)]
+    pub stencil: Option<Handle<Image>>,
 }
 
-impl Material for WormholeMaterial {
+impl Material for WormholeSurface {
     fn fragment_shader() -> ShaderRef {
         "embedded://bevy_wormholes/surface.wgsl".into()
     }
