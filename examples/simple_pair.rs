b9ec1f6e555f9a5fd40417e5615bff7d01fa158b
veritius <veritiusgaming@gmail.com>
2024-04-19 12:02:43 +1000

    Flying camera

--- examples/simple_pair.rs
+++ examples/simple_pair.rs
@@ -1,11 +1,15 @@
-use bevy::prelude::*;
+use bevy::{input::mouse::MouseMotion, prelude::*, window::{CursorGrabMode, PrimaryWindow}};
 use bevy_wormholes::*;
+
+const MOUSE_SENSITIVITY: Vec2 = Vec2::new(0.001, 0.001);
+const CAMERA_MOVE_SPEED: f32 = 1.0;
 
 fn main() {
     let mut app = App::new();
     app.add_plugins((DefaultPlugins, WormholesPlugin));
 
-    app.add_systems(Startup, spawn_props);
+    app.add_systems(Startup, (spawn_props, setup_camera));
+    app.add_systems(Update, flycam_system);
 
     app.run();
 }
@@ -40,4 +44,94 @@ fn spawn_props(
         transform: Transform::from_xyz(6.0, 1.0, 2.0),
         ..default()
     });
+}
+
+#[derive(Component)]
+struct Flycam {
+    lock: bool,
+    rotation: Vec2,
+}
+
+impl Default for Flycam {
+    fn default() -> Self {
+        Self {
+            lock: true,
+            rotation: Vec2::ZERO,
+        }
+    }
+}
+
+fn setup_camera(
+    mut commands: Commands,
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    // Spawn camera
+    commands.spawn((
+        Flycam::default(),
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 3.0, 10.0),
+            ..default()
+        },
+    ));
+
+    // Lock cursor
+    let mut window = windows.single_mut();
+    window.cursor.grab_mode = CursorGrabMode::Locked;
+    window.cursor.visible = false;
+}
+
+fn flycam_system(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut mouse: EventReader<MouseMotion>,
+    mut camera: Query<(&mut Flycam, &mut Transform)>,
+    mut windows: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    // Get the camera components
+    let (mut camera, mut transform) = camera.single_mut();
+
+    // Toggle locking
+    if keyboard.just_pressed(KeyCode::Escape) {
+        camera.lock = !camera.lock;
+
+        let mut window = windows.single_mut();
+        match camera.lock {
+            true => {
+                window.cursor.grab_mode = CursorGrabMode::Locked;
+                window.cursor.visible = false;
+            },
+            false => {
+                window.cursor.grab_mode = CursorGrabMode::None;
+                window.cursor.visible = true;
+            },
+        }
+    }
+
+    // Only move if locked
+    if !camera.lock { return }
+
+    // Turning inputs
+    for delta in mouse.read().map(|motion| motion.delta) {
+        camera.rotation += delta * MOUSE_SENSITIVITY;
+        let rot_yaw = Quat::from_axis_angle(Vec3::Y, -camera.rotation.x);
+        let rot_pit = Quat::from_axis_angle(Vec3::X, -camera.rotation.y);
+        transform.rotation = rot_yaw * rot_pit;
+    }
+
+    // Direction based on rotation
+    let up = *transform.up();
+    let fwd = *transform.forward();
+    let rgt = *transform.right();
+
+    // Keyboard inputs
+    let mut movement = Vec3::ZERO;
+    if keyboard.pressed(KeyCode::KeyW       ) { movement += fwd }
+    if keyboard.pressed(KeyCode::KeyS       ) { movement -= fwd }
+    if keyboard.pressed(KeyCode::KeyA       ) { movement -= rgt }
+    if keyboard.pressed(KeyCode::KeyD       ) { movement += rgt }
+    if keyboard.pressed(KeyCode::Space      ) { movement += up  }
+    if keyboard.pressed(KeyCode::ControlLeft) |
+        keyboard.pressed(KeyCode::ShiftLeft) { movement -= up  }
+
+    // Apply movement
+    transform.translation += movement * CAMERA_MOVE_SPEED;
 }
